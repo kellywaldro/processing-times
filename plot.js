24ce907ed0b6aba57c9ae86c1ff4746f774c6f38
kellyawaldron <116761579+kellywaldro@users.noreply.github.com>
2023-05-02 23:58:39 -0400

    trying to get the dropdown to work

--- plot.js
+++ plot.js
@@ -1,6 +1,13 @@
 
+
 line_1.visible = true
 line_2.visible = true
+line_3.visible = true
+line_4.visible = true
+line_5.visible = true
+line_6.visible = true
+line_7.visible = true
+line_8.visible = true
 
 if (this.value === "Line 1") {
     line_2.visible = false 
